dcb3349780af3c05faeb9aeb2c9a0ff38557dfa2
mdsingh007 <mdsingh@gmail.com>
2020-05-31 22:29:07 -0500

    new files

--- SmallScripts/debug_me_1.py
+++ SmallScripts/debug_me_1.py
@@ -9,25 +9,41 @@ def name_value(name):
         tot_val += ord(elem)
     return tot_val
 
+def reader(lines):
+    names = {}
+    for l in lines:
+        if l in names:
+            names[l] += 1
+        else:
+            names[l] = 1
+    return names
+
+
 print( name_value('Manish') )
 print( name_value('Kushagra') )
 print( name_value('Mridula') )
 
 name = open('SmallScripts/names.txt')
 
-print(name)
+# print(name)
 
 txt = name.read()
+list_txt = txt.split()
 maximum = ''
+minimum = list_txt[0]
 # txt.split()
 # print(txt)
 # print(txt.split())
 
-for elem in txt.split():
+for elem in list_txt:
     print(elem, name_value(elem))
     if name_value(elem) > name_value(maximum):
         maximum = elem
+    if name_value(elem) < name_value(maximum):
+        maximum = elem
+print(reader(list_txt))
     
 print("highest number is", maximum, name_value(maximum))
+print("lowest number is", minimum, name_value(minimum))
 
 
