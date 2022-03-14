3c1efec9a6f8b064ff2b6ec328d3947b93ca08fa
kushu <mdsingh@gmail.com>
2022-03-14 15:57:47 -0500

    showing chat messages

---
+++ tk-gui/Kchat/dbops.py
@@ -0,0 +1,32 @@
+import sqlite3
+import sys
+import os
+
+DB_NAME = 'kchat.db'
+DB_PATH = os.path.join( os.getcwd(), 'Kchat', DB_NAME )
+# if db does not exist, exit instead of creating one
+if not os.path.exists(DB_PATH):
+    sys.exit(f"database does not exist. lookup failed for - {DB_PATH}")
+else:
+    print (f'connecting to {DB_PATH}')
+
+con = sqlite3.connect(DB_PATH)
+
+def execute_sql(query):
+    cursorObj = con.cursor()
+    # print(query)
+    cursorObj.execute(query)
+    con.commit()
+    return cursorObj
+
+def select_all():
+    sql = 'select * from chats'
+    return execute_sql(sql).fetchall()
+
+
+def new(datetime, sender, receiver, message):
+    sql = f"INSERT INTO chats (datetime, sender, receiver, message) VALUES('{datetime}', '{sender}', '{receiver}' , '{message}')"
+    execute_sql(sql)
+
+# if __name__ == '__main__':
+#     select_all()
--- tk-gui/Kchat/kchat.py
+++ tk-gui/Kchat/kchat.py
@@ -1,16 +1,58 @@
 import PySimpleGUI as sg
+from dbops import select_all
 
-layout = [[sg.Text('Welcome to Kchat!')]]
+sg.theme('DarkTeal2')   # Add a touch of color
 
-window = sg.Window('Kchat', 
-    [[
-        sg.Column(layout, key='listview')
-    ]]
-)
+btnSize = (6,1)
+# # heading = 'Kchat'
+friend_view = [ [sg.Text('Friends')], 
+                [sg.Button('Kanha', size=btnSize)], 
+                [sg.Button('Papa', size=btnSize)], 
+                [sg.Button('Mama', size=btnSize)] ]
+# message_view = [[sg.Text('Messages', size = (40, 1))]]
 
+layout = [[sg.Output(size=(80, 20), key='-messages-')],
+            [sg.Multiline(size=(70, 4), enter_submits=True, key='-text-'),
+            sg.Button('SEND', button_color=(sg.YELLOWS[0], sg.BLUES[0])),
+         ]]
+
+window = sg.Window('Kchat', [[
+        sg.Column(friend_view, key='-friend_view-'),
+        sg.Column(layout, key='-message_view-')
+        ]])
+
+
+# while True:
+#     event, values = window.read()
+    # if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
+    #     break
+
+# window.close()
+buttons = ('Kanha', 'Papa', 'Mama')
+    # ---===--- Loop taking in user input and using it to query HowDoI web oracle --- #
 while True:
     event, values = window.read()
+
+    if event == 'SEND':
+        # print(values['-text-'])
+        window['-text-'].update(value='')
+
     if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
         break
 
+    if event in buttons:
+        for btn in buttons:
+            window[btn].update(button_color=sg.theme_button_color())
+        window[event].update(button_color='Grey')
+        window['-messages-'].update(value='')
+
+        for elem in buttons:
+            if event == elem:
+                allmsg = select_all()
+                for msg in allmsg:
+                    if msg[2] == elem and msg[3] == 'Kashvi':
+                        print (f"{elem}: {msg[4]}")
+
+        window.refresh()
+    # print(event)
 window.close()
